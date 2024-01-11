c75273db27ad1188f4065022b73589387e0a0d5f
Denis Savchenko <msc.savchenko.d@gmail.com>
2024-01-11 17:03:30 +0300

    fix: add main queue

--- operations/Operation_ДЗ/DownloadImage.swift
+++ operations/Operation_ДЗ/DownloadImage.swift
@@ -1,35 +1,42 @@
+
 import Foundation
 import UIKit
 
 let cache = NSCache<AnyObject, AnyObject>()
-
 class DownloadImage: Operation {
-    
+
     static let shared = DownloadImage()
-    
-    private var task: URLSessionTask?
-    
-    func loadImage(from url: URL, completion: @escaping (UIImage?) -> Void) {
-        if let imageFromCache = cache.object(forKey: url.absoluteString as AnyObject) as? UIImage {
-            completion(imageFromCache)
-            return
-        }
-        
+    let imageView = CustomImageView()
+
+    private override init() {}
+
+    var task: URLSessionTask!
+    var operationQueue = OperationQueue()
+
+    func loadImage(from url: URL) {
+
+        imageView.image = nil
+
         if let task = task {
             task.cancel()
         }
-        
-        task = URLSession.shared.dataTask(with: url) { data, response, error in
-            guard let data = data, let image = UIImage(data: data) else {
-                completion(nil)
+
+        operationQueue.addOperation {
+            if let imageFromCache = cache.object(forKey: url.absoluteString as AnyObject) as? UIImage {
+                self.imageView.image = imageFromCache
                 return
             }
-            
-            cache.setObject(image, forKey: url.absoluteString as AnyObject)
-            DispatchQueue.main.async {
-                completion(image)
-            }
+
+            self.task = URLSession.shared.dataTask(with: url, completionHandler: { data, response, error in
+                guard let data = data, let image = UIImage(data: data) else { return }
+
+                cache.setObject(image, forKey: url.absoluteString as AnyObject)
+                DispatchQueue.main.async {
+                    self.imageView.image = image
+                }
+            })
+            self.task.resume()
         }
-        task?.resume()
     }
+
 }
